3358ef5ec7f9a38e4c7ed53d80086802e0ecb280
Luis Hernandez <155981836+luishdzl@users.noreply.github.com>
2025-04-08 23:24:31 -0400

    correcion

--- src/Providers/QuoteServiceProvider.php
+++ src/Providers/QuoteServiceProvider.php
@@ -24,10 +24,10 @@ class QuoteServiceProvider extends ServiceProvider
             __DIR__.'/../../config/quotes.php' => config_path('quotes.php'),
         ], 'config');
 
-        // Publica los assets de la UI de Vue.js (se publicarán en public/vendor/my-quotes-package)
-        $this->publishes([
-            __DIR__.'/../../resources/dist' => public_path('vendor/my-quotes-package'),
-        ], 'public');
+    // Publica los assets de la UI (JS/CSS)
+    $this->publishes([
+        __DIR__.'/../../public' => public_path('vendor/my-quotes-package'),
+    ], 'public'); // Tag "public"
 
         // Carga las rutas del paquete: API y Web
         $this->loadRoutesFrom(__DIR__.'/../../src/routes/api.php');
